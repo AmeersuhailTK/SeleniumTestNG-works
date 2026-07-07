fd7bb528d4315bf6dbb5e91cd8de090ac12f69a5
ameersuahiltk <disneywelcome786@gmail.com>
2026-07-07 10:45:57 +0300

    First commit

---
+++ SeleniumTestNGProjects/src/startWithNG/NewTest.java
@@ -0,0 +1,24 @@
+package startWithNG;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void firstMethod() {
+	  
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  
+  }
+
+}
